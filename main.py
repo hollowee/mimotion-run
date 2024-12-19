94428b65c87bd4b585e466c771a80b4995e1375d
Chen He <2622373025@qq.com>
2024-12-19 10:12:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -203,7 +203,7 @@ class MiMotion():
             print("初始化步数失败: 已将最大值设置为 19999", e)
             max_step = 19999
 
-        step = str(8000)
+        step = str(9000)
         if ("+86" in user) or "@" in user:
             user = user
         else:
